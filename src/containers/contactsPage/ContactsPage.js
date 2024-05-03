562e1a84d53c0999ec0e3afa341b204eda0060ad
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-03 10:42:07 +0100

    contacts page: added statefulness, check for duplicate entries,

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,25 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const contacts=props.contacts;
+  const addContact=props.addContact;
+  const [name,setName] = useState("");
+  const [number,setNumber] = useState("");
+  const [email,setEmail] = useState("");
+  const [nameDuplicate, setNameDuplicate]=useState(false);
+
+  useEffect(()=>{
+    name.forEach((item)=>{
+      if(contacts.contains(item)){
+        setNameDuplicate(true);
+      }
+    })
+  },[contacts,name])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +29,13 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if(nameDuplicate===false){
+      addContact(name,number,email);
+      setName("");
+      setNumber("");
+      setEmail("");
+
+    }
   };
 
   /*
