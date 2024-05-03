2a9f65631f36df1724c601b4834565b36af87b08
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-03 11:04:03 +0100

    added rendering to contacts page and passed down props

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -4,54 +4,69 @@ import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
 export const ContactsPage = (props) => {
-  /*
+    /*
   Define state variables for 
   contact info and duplicate check
   */
-  const contacts=props.contacts;
-  const addContact=props.addContact;
-  const [name,setName] = useState("");
-  const [number,setNumber] = useState("");
-  const [email,setEmail] = useState("");
-  const [nameDuplicate, setNameDuplicate]=useState(false);
+    const contacts = props.contacts;
+    const addContact = props.addContact;
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [nameDuplicate, setNameDuplicate] = useState(false);
 
-  useEffect(()=>{
-    name.forEach((item)=>{
-      if(contacts.contains(item)){
-        setNameDuplicate(true);
-      }
-    })
-  },[contacts,name])
+    useEffect(
+        (name) => {
+            contacts.forEach((contactObject) => {
+                if (contactObject.name === name) {
+                    setNameDuplicate(true);
+                }
+            });
+        },
+        [contacts, name]
+    );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /*
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
-    if(nameDuplicate===false){
-      addContact(name,number,email);
-      setName("");
-      setNumber("");
-      setEmail("");
+        if (nameDuplicate === false) {
+            addContact(name, number, email);
+            setName("");
+            setNumber("");
+            setEmail("");
+        }
+    };
 
-    }
-  };
-
-  /*
+    /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
 
-  return (
-    <div>
-      <section>
-        <h2>Add Contact</h2> 
-      </section>
-      <hr />
-      <section>
-        <h2>Contacts</h2>
-      </section>
-    </div>
-  );
+    return (
+        <div>
+            <section>
+                <h2>Add Contact</h2>
+                <ContactForm
+                    name={name}
+                    setname={setName}
+                    number={number}
+                    setnumber={setNumber}
+                    email={email}
+                    setEmail={setEmail}
+                    nameduplicate={nameDuplicate}
+                    handleSubmit={handleSubmit}
+                />
+            </section>
+            <hr />
+            <section>
+                <h2>Contacts</h2>
+                <TileList 
+                contacts={contacts}
+                />
+            </section>
+        </div>
+    );
 };
