ab9d9a3eb0b4570d2e40444d5c815be8af1c12c7
Seb Davies <seb_davies@hotmail.co.uk>
2024-07-10 22:55:22 +0100

    address eslint issues

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -4,48 +4,49 @@ import { AppointmentForm } from "../../components/appointmentForm/AppointmentFor
 import { TileList } from "../../components/tileList/TileList";
 
 export const AppointmentsPage = (props) => {
-    /*
+  /*
   Define state variables for 
   appointment info
   */
-    const appointments = props.appointments;
-    const contacts = props.contacts;
-    const addAppointment = props.addAppointment;
-    const [name, setName] = useState("");
-    const [contact, setContact] = useState("");
-    const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
+  const appointments = props.appointments;
+  const addAppointment = props.addAppointment;
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addAppointment(name, date, time);
-        /*
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addAppointment(name, date, time);
+    /*
     Add contact info and clear data  
     */
-        setName("");
-        setDate("");
-        setTime("");
-    };
+    setName("");
+    setDate("");
+    setTime("");
+  };
 
-    return (
-        <div>
-            <section>
-                <h2>Add Appointment</h2>
-                <AppointmentForm name={name}
-                setName={setName} 
-                contact={contact}
-                setContact={setContact}
-                date={date}
-                setDate={setDate}
-                time={time}
-                setTime={setTime}
-                handleSubmit={handleSubmit} />
-            </section>
-            <hr />
-            <section>
-                <h2>Appointments</h2>
-                <TileList object={appointments} />
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <section>
+        <h2>Add Appointment</h2>
+        <AppointmentForm
+          name={name}
+          setName={setName}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+        />
+      </section>
+      <hr />
+      <section>
+        <h2>Appointments</h2>
+        <TileList object={appointments} />
+      </section>
+    </div>
+  );
 };
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -4,67 +4,67 @@ import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
 export const ContactsPage = (props) => {
-    /*
+  /*
   Define state variables for 
   contact info and duplicate check
   */
-    const contacts = props.contacts;
-    const addContact = props.addContact;
-    const [name, setName] = useState("");
-    const [number, setNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [nameDuplicate, setNameDuplicate] = useState(false);
+  const contacts = props.contacts;
+  const addContact = props.addContact;
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [nameDuplicate, setNameDuplicate] = useState(false);
 
-    useEffect(
-        (name) => {
-            Object.values(contacts).map((contact, index) => {
-                if (contact[0] === name) {
-                    setNameDuplicate(true);
-                }
-            });
-        },
+  useEffect(
+    (name) => {
+      Object.values(contacts).map((contact, index) => {
+        if (contact[0] === name) {
+          return setNameDuplicate(true);
+        }
+      });
+    },
 
-        [contacts, name]
-    );
+    [contacts, name]
+  );
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        /*
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
-        if (nameDuplicate !== false) {
-            addContact(name, number, email);
-            setName("");
-            setNumber("");
-            setEmail("");
-        }
-    };
+    if (nameDuplicate !== false) {
+      addContact(name, number, email);
+      setName("");
+      setNumber("");
+      setEmail("");
+    }
+  };
 
-    /*
+  /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
 
-    return (
-        <div>
-            <section>
-                <h2>Add Contact</h2>
-                <ContactForm
-                    name={name}
-                    setName={setName}
-                    phone={number}
-                    setPhone={setNumber}
-                    email={email}
-                    setEmail={setEmail}
-                    handleSubmit={handleSubmit}
-                />
-            </section>
-            <hr />
-            <section>
-                <h2>Contacts</h2>
-                <TileList object={contacts} />
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={number}
+          setPhone={setNumber}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+        <TileList object={contacts} />
+      </section>
+    </div>
+  );
 };
